572f6d8d0b79a50b05c553a13900e50a5c119490
5gtp3pp3r <william.crepault@gmail.com>
2025-05-18 17:48:56 -0400

    Reformulation SubmitConfirm avec SubmitConfirmPage et PageTemplate

---
+++ rainbowtattoos/src/pages/submitConfirm/SubmitConfirm.tsx
@@ -0,0 +1,13 @@
+import { JSX } from 'react';
+import { Link } from 'react-router-dom';
+
+
+export function SubmitConfirm(): JSX.Element {
+    
+    return (
+        <>
+            <p> marci</p>
+            <Link to="/" >Retour à l'accueil</Link>
+        </>
+    )
+}
---
+++ rainbowtattoos/src/pages/submitConfirm/SubmitConfirmPage.tsx
@@ -0,0 +1,16 @@
+import { SubmitConfirm } from './SubmitConfirm';  
+import { PageTemplate } from '../../PageTemplate';
+import { JSX } from 'react';
+
+export function SubmitConfirmPage(): JSX.Element {
+  return (
+    <PageTemplate 
+      previous="/" 
+      nextPage="/" 
+      bgImageClass="bg-link"
+      txtColor="beige"
+    >
+      {<SubmitConfirm />}
+    </PageTemplate>
+  );
+}
