0331559ba549ee930c0504c4191ac596848be38c
5gtp3pp3r <william.crepault@gmail.com>
2025-05-18 17:50:48 -0400

    Ajout des pages 404 et submitConfirm dans App

--- rainbowtattoos/src/App.tsx
+++ rainbowtattoos/src/App.tsx
@@ -7,13 +7,13 @@ import "./styles.css";
 import { JSX } from 'react';
 import { NavBarre } from './NavBarre';
 import { Footer } from './Footer';
-import { Page404 } from './pages/Page404';
-
 import { ScrollToTop } from "./ScrollToTop";
-import { HomePage } from './pages/home/homePage'; 
+import { HomePage } from './pages/home/HomePage'; 
 import { PortfolioPage } from './pages/portfolio/PortfolioPage';
 //import { TattoosPage } from './pages/tattoos/TattoosPages';
 import { ContactsPage } from './pages/contacts/ContactsPage';
+import { SubmitConfirmPage } from './pages/submitConfirm/SubmitConfirmPage';
+import { Page404Page } from './pages/page404/Page404Page';
 
 
 
@@ -26,10 +26,11 @@ export function App(): JSX.Element {
         <NavBarre />        
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="*" element={<Page404 />} />
+            <Route path="*" element={<Page404Page />} />
             <Route path="pages/portfolio" element={<PortfolioPage />} />
             {/*<Route path="pages/tattoos" element={<TattoosPage />} />*/}
             <Route path="pages/contacts" element={<ContactsPage />} />
+            <Route path="pages/submitConfirm" element={<SubmitConfirmPage />} />
           </Routes>        
         <Footer />
       </BrowserRouter>
